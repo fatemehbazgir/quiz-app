746cba832d4e903cc86429f11fbca41ead2399bf
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-11-17 17:35:16 +0330

    Show questions and answers

--- js/game.js
+++ js/game.js
@@ -1,10 +1,14 @@
 import formatData from "./helper.js";
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 const URL =
   "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
 
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
@@ -13,8 +17,21 @@ const fetchData = async () => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
-
+const checkAnswer = () => {};
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
 window.addEventListener("load", fetchData);
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
