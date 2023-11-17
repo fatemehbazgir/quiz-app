2adb0fe866da9be41c30dee10950c4ab87c76c48
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-11-17 18:24:29 +0330

    Next button, go to the next question

--- js/game.js
+++ js/game.js
@@ -4,6 +4,8 @@ const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
 const scoreText = document.getElementById("score");
+const nextButton = document.getElementById("next-button");
+const questionNumber = document.getElementById("question-number");
 const URL =
   "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
 
@@ -39,6 +41,7 @@ const checkAnswer = (event, index) => {
   }
 };
 const showQuestion = () => {
+  questionNumber.innerText = questionIndex + 1;
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
   correctAnswer = correctAnswerIndex;
@@ -47,7 +50,21 @@ const showQuestion = () => {
     button.innerText = answers[index];
   });
 };
+const nextHandler = () => {
+  questionIndex++;
+  if (questionIndex < formattedData.length) {
+    isAccepted = true;
+    removeClass();
+    showQuestion();
+  } else {
+    window.location.assign("/end.html");
+  }
+};
+const removeClass = () => {
+  answerList.forEach((button) => (button.className = "answer-text"));
+};
 window.addEventListener("load", fetchData);
+nextButton.addEventListener("click", nextHandler);
 answerList.forEach((button, index) => {
   button.addEventListener("click", (event) => checkAnswer(event, index));
 });
