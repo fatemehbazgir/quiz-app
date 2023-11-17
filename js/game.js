a8dfa7f96d7973dc1a224e0a7059a6a7ba61ad67
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-11-17 18:05:18 +0330

    Score each question

--- js/game.js
+++ js/game.js
@@ -3,12 +3,16 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
 const URL =
   "https://opentdb.com/api.php?amount=20&difficulty=medium&type=multiple";
 
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+const CORRECT_BONUS = 10;
+let isAccepted = true;
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
@@ -22,9 +26,13 @@ const start = () => {
   container.style.display = "block";
 };
 const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
   const isCorrect = index === correctAnswer ? true : false;
   if (isCorrect) {
     event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
   } else {
     event.target.classList.add("incorrect");
     answerList[correctAnswer].classList.add("correct");
