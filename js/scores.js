d2ddbe1785f2b70908f916149b0a7802a5d1a4a8
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2023-11-21 16:45:03 +0330

    Show the user with the highest score

---
+++ js/scores.js
@@ -0,0 +1,12 @@
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+const list = document.querySelector("ol");
+const content = highScores.map((score, index) => {
+  return `
+    <li>
+    <span>${index + 1}</span>
+    <p>${score.name}</p>
+    <span>${score.score}</span>
+    </li>
+    `;
+});
+list.innerHTML = content.join("");
